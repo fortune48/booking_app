4f9998f3f0811c634166184b37feda7b05f3dbea
Fortune <ukwuomafortune01@gmail.com>
2022-07-06 12:21:26 +0100

    seeded users(doctor) to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  User.create!(email: 'doc1@test.com', password: 'password1', password_confirmation: 'password1', role: 'doctor', gender: 'male',
+               first_name: 'John', last_name: 'Doe', age: 45)
+  User.create!(email: 'doc2@test.com', password: 'password1', password_confirmation: 'password1', role: 'doctor', 
+               gender: 'female', first_name: 'Lisa', last_name: 'Jelena', age: 40)
+  
+  
+  
